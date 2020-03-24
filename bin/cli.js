6130d82ab68c8d9c4134fb9bcdbf096d6f65390b
PoppinL <poppinlp@gmail.com>
2020-03-24 19:25:49 +0800

    fix bin; add help

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,24 @@
-const assert = require('assert').strict;
+#!/usr/bin/env node
+
+const assert = require('assert');
 const argv = require('minimist')(process.argv.slice(2));
 const { getBvInfo } = require('../');
+
+if (!argv.length || argv.h || argv.help) {
+  console.log(`
+Usage
+  $ bv2av [<args>]
+Options
+  --bv          The bv code
+  --all         Get all bv infomations
+  -h,--help     Show help
+Samples
+  $ bv2av --bv=xxxxxx -all
+  `);
+  return;
+}
+
+return;
 
 assert.notStrictEqual(argv.bv, undefined, 'Must input bv code!');
 assert.strictEqual(typeof argv.bv, 'string', 'BV code must be a string!');
